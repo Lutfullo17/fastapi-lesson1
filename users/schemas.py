dbe7449e26550c8761c13e215bfcdd8f24d4c81f
Lutfullo08 <lutfullorizayev222@gmail.com>
2026-04-19 10:03:46 +0500

    646555

---
+++ users/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel,Field
+from typing import Optional
+
+
+
+class SignUpSchema(BaseModel):
+    first_name :Optional[str]
+    username :str
+    email :str
+    password :str
+
+class LoginSchema(BaseModel):
+    username: str
+    password :str
--- users/urls.py
+++ users/urls.py
@@ -1,29 +1,49 @@
-from fastapi import APIRouter, Request
-
-router =  APIRouter(prefix="/users")
-
-@router.get('/')
-async def users_list():
-    return {'Laziz, Aziz'}
-
-@router.get('/user1')
-async def users_1():
-    return {'Lutfullo Rizoyev'}
+from sqlalchemy import select
+from users.models import User
+from fastapi import APIRouter,Depends,status
+from fastapi.exceptions import HTTPException
+from users.schemas import SignUpSchema,LoginSchema
+from sqlalchemy.ext.asyncio import AsyncSession
+from db import get_db
+from users.auth import hash_password
+from users.crud import login_crud
 
 
-
-@router.post("/data")
-async def yubotr(request: Request):
-    data = await request.json()
-    return {'title': data.get('title'), 'price': data.get('price')}
+router=APIRouter(prefix='/user',tags=['auth'])
 
 
-from pydantic import BaseModel
+@router.post("/sign-up")
+async def sign_up(user:SignUpSchema,db:AsyncSession=Depends(get_db)):
+    username=await db.execute(select(User).where(User.username==user.username))
+    email=await db.execute(select(User).where(User.email==user.email))
+    db_username=username.scalar_one_or_none()
+    db_email=email.scalar_one_or_none()
 
-class User(BaseModel):
-    name: str
-    age: int
-@router.post("/data1")
-async def yuborish(user:User):
-    return {"name": user.name, "age": user.age}
+    if db_username:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='bu username band')
+    if db_email:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='bu email band')
+    user=User(
+        first_name=user.first_name,
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
 
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    response={
+        'status':status.HTTP_201_CREATED,
+        "message":"ro'yxatdan o'tdiz",
+        "data":{
+            "username":user.username,
+            "email":user.email
+        }
+    }
+    return response
+
+
+@router.post("/login")
+async def login(user:LoginSchema,db:AsyncSession=Depends(get_db)):
+    return await login_crud(user,db)
